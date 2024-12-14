bb1bfaaa7ebade677383ad7fc76ee3b0c063db09
ProgSomel <somelahmed55@gmail.com>
2024-12-14 11:20:48 +0600

    User Model added

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,7 @@
 export type TUser = {
   id: string;
   password: string;
-  needsPasswordChange: string;
+  needsPasswordChange: boolean;
   role: "admin" | "student" | "faculty";
   isDeleted: boolean;
   status: "in-progress" | "blocked";
